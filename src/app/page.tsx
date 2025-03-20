ff735b506c18eb1c537608d4f8e486140657611d
Xiao <sunxiaolive2016@outlook.com>
2025-03-20 11:59:50 +0100

    feat: remove customized card component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,9 @@
 import Image from "next/image";
-import { DashboardCard } from "./components/card";
 
 export default function Home() {
   return (
     <>
       <h1>Dashboard</h1>
-      <DashboardCard title={"Card title"} value={"Card value"} />
     </>
   );
 }
